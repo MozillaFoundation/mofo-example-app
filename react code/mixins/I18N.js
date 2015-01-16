43c025e87006507e79eeaa1fef2f54032e9e3cd6
Pomax <pomax@nihongoresources.com>
2015-01-15 22:44:22 -0800

    small refactor

--- react code/mixins/I18N.js
+++ react code/mixins/I18N.js
@@ -1,35 +1,4 @@
-// this would be a library requirement after we fully implement l10n/i18n
-var localizers = {};
-
-var defaultLocalizer = function(v) { return v; }
-localizers["en-US"] = defaultLocalizer;
-localizers["en-US-reverse"] = function(v) { return v.split('').reverse().join(''); };
-
-var L10N = {
-  locale: localStorage["mofo-example-app-locale"] || "en-US",
-
-  locales: {
-    "en-US": "US English",
-    "en-US-reverse": "Mirrorlandish"
-  },
-
-  localize: defaultLocalizer,
-
-  setLocale: function(locale) {
-    locale = locale || localStorage["mofo-example-app-locale"];
-    this.locale = locale;
-    this.localize = localizers[locale] || defaultLocalizer;
-    localStorage["mofo-example-app-locale"] = locale;
-  },
-
-  localizeAll: function(map) {
-    var mapped = {};
-    Object.keys(map).forEach(function(key) {
-      mapped[key] = L10N.localize(map[key]);
-    });
-    return mapped;
-  }
-};
+var L10N = require("./../../lib/L10N");
 
 var LocaleEventName = "locale-change";
 
